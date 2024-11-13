2985f17a7e9b9ac74be913e802b3485422758c5b
Elcid-Chawa <elcidchawa@gmail.com>
2024-11-13 23:35:48 +0200

    fixing server request time

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 10000, // 10 seconds
+    socketTimeoutMS: 45000, // 45 seconds
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
