38fc9cadfb284d0c5352f2558383dc70302519c9
Elcid-Chawa <elcidchawa@gmail.com>
2024-11-29 17:21:13 +0200

    fixed course update

--- routes/courses/index.js
+++ routes/courses/index.js
@@ -29,10 +29,8 @@ router.get("/", async (req, res) => {
 router.post("/update-courses", async (req, res) => {
   const headers = {
     accept: "application/json",
-    "Content-Type": "application/json",
     Authorization: apiKey,
   };
-
   try {
     // Fetch courses from Illumidesk
     const response = await axios.get(apiUrl, { headers });
@@ -40,8 +38,8 @@ router.post("/update-courses", async (req, res) => {
     if (response.status !== 200) {
       res.json({ message: "Failed to fetch courses from Illumidesk" });
     } else {
-      const illumidesk_courses = response.data;
-
+      const illumidesk_courses = await response.data.results;
+      console.log("Fetching courses from Illumidesk: ", illumidesk_courses);
       for (const course of illumidesk_courses) {
         // Assuming `course_id` is the unique identifier
         const course_id = course.uuid; // Replace with the actual unique identifier
@@ -49,18 +47,18 @@ router.post("/update-courses", async (req, res) => {
         if (!course_id) {
           return res.json({ message: "Missing uuid in course data" });
         }
-
-        await Courses.replaceOne(
+        const { cost, ...otherFields } = course;
+        await Courses.updateOne(
           { uuid: course_id }, // Find course by unique ID
-          course, // Replace the document with new data
-          (upsert = true), // Insert if it doesn't exist
+          { $set: otherFields }, // Replace the document with new data
+          {upsert: true}, // Insert if it doesn't exist
         );
       }
 
       res.status(200).json({ message: "Updated courses successfully" });
     }
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     res.status(500).json({ error: "Internal server error " + error.message });
   }
 });
